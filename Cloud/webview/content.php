f2b9327fcf0183db546a230e9d0dc07fe7663373
calculus <kalanah1996@gmail.com>
2019-10-04 15:55:57 +0530

    All done!

--- Cloud/webview/content.php
+++ Cloud/webview/content.php
@@ -63,13 +63,28 @@
               </a> -->
             </div>
           </div>
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-success o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-life-ring"></i>
+                </div>
+                <div class="mr-5"><h3><?php echo 'Server Clock: ';echo $date = date('h:i:s a', time()); ?></h3></div>
+              </div>
+            <!--   <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a> -->
+            </div>
+          </div>
         </div>
 
         <!-- DataTables Example -->
         <div class="card mb-3">
           <div class="card-header">
-            <i class="fas fa-table"></i>
-            Report</div>
+            Report 
           <div class="card-body">
             <div class="table-responsive">
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
