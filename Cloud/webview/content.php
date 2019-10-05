fcb07d25704a7c4685c9d599a2e3ab09ba8bdfcb
calculus <kalanah1996@gmail.com>
2019-10-05 21:11:29 +0530

    Web view webview improvements last update card| equilavant username display| user status!

--- Cloud/webview/content.php
+++ Cloud/webview/content.php
@@ -19,6 +19,23 @@
 
 ?>
 
+          <?php 
+                
+                $sql = "SELECT * FROM user_attendance ORDER BY id DESC LIMIT 1 ";  
+                $res = $conn->query($sql);
+                while($row1 = $res->fetch_assoc()) {
+                  $lastupdate = $row1["timestamp"];
+                  $lastpersonstatus = $row1["allow"];
+                  $lastpersonid = $row1["id"];
+                  if ($lastpersonstatus=='1') {
+                      $lastpersonstatus = "Allowed";
+                  }else{
+                      $lastpersonstatus = "Disallowed";
+                  }
+                }
+
+           ?>
+
 
  <!-- Breadcrumbs-->
         <ol class="breadcrumb">
@@ -34,7 +51,7 @@
             <div class="card text-white bg-primary o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
-                  <i class="fas fa-fw fa-comments"></i>
+                  <i class="fas fa-fw fa-life-ring"></i>
                 </div>
                 <div class="mr-5"><h3><?php echo $count; ?> Allowed Students</h3></div>
               </div>
@@ -51,7 +68,7 @@
             <div class="card text-white bg-danger o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
-                  <i class="fas fa-fw fa-life-ring"></i>
+                  <i class="fas fa-fw fa-tachometer-alt"></i>
                 </div>
                 <div class="mr-5"><h3><?php echo $count2; ?> Disallowed Students</h3></div>
               </div>
@@ -67,22 +84,39 @@
             <div class="card text-white bg-success o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
-                  <i class="fas fa-fw fa-life-ring"></i>
+                  <i class="fas fa-fw fa-table"></i>
                 </div>
                 <div class="mr-5"><h3><?php echo 'Server Clock: ';echo $date = date('h:i:s a', time()); ?></h3></div>
               </div>
-            <!--   <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">View Details</span>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left"><h6><?php echo date('d F, Y (l)'); ?></h6></span>
                 <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
+<!--                   <i class="fas fa-angle-right"></i> -->
                 </span>
-              </a> -->
+              </a>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-info o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-user"></i>
+                </div>
+                <div class="mr-5"><h3>Last updated at:<br><?php echo $lastupdate; ?></h3></div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left"><h6>Status: <?php echo $lastpersonstatus; ?></h6></span>
+                <span class="float-right">
+                  <!-- <i class="fas fa-angle-right"></i> -->
+                </span>
+              </a>
             </div>
           </div>
         </div>
 
+
         <!-- DataTables Example -->
-        <div class="card mb-3">
+        <div class="card mb-3"> 
           <div class="card-header">
             Report  
           <div class="card-body">
@@ -111,7 +145,13 @@
                     while($row = mysqli_fetch_array($result)){
                       echo "<tr>";
                       echo "<td>" . $row['id'] . "</td>";
-                      echo "<td>" . $row['userid'] . "</td>";
+                      
+                      if($row['userid']=='17121543'){
+                        echo "<td>Kalana Hettiarachchi</td>";
+                      }else{
+                        echo "<td class='text-danger'>" . $row['userid'] . "(Unregistered User)"."</td>";;
+                      }
+
                       if($row['allow']==1){
                         echo "<td class='text-center'><button type='button' class='btn btn-success'>" . $row['allow'] . "</button></td>";
                       }else{
@@ -129,34 +169,10 @@
             </div>
           </div>
 
-          <?php 
-                
-                $sql = "SELECT * FROM user_attendance ORDER BY id DESC LIMIT 1 ";  
-                $res = $conn->query($sql);
-                while($row1 = $res->fetch_assoc()) {
-                  $lastupdate = $row1["timestamp"];
-                }
 
-           ?>
           
 
           <div class="card-footer small text-muted">Last updated at <?php echo $lastupdate; ?></div>
         </div>
+      </div>
 
-
-<!-- <script>
-    $(document).ready(function () {
-        var table = $('#dataTable').DataTable({
-            "paging": false,
-            "processing": true,
-            "serverSide": true,
-            'serverMethod': 'post',
-            "ajax": "server.php",
-            dom: 'Bfrtip',
-            buttons: [
-                {extend: 'copy', attr: {id: 'allan'}}, 'csv', 'excel', 'pdf'
-            ]
-        });
-
-    });
-</script> -->
